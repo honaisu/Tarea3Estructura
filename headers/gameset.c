96ae324697289f0672f3702c77c6c4559d958d10
honaisu <iruiz.aran@gmail.com>
2025-05-24 15:20:27 -0400

    Añadida la lógica para detectar el final; Mejora de lógica general en algunos parametros de las funciones del jugador. También, se agrego la opción de decidir en cada opción que lo necesitara (resetear y salir)

--- headers/gameset.c
+++ headers/gameset.c
@@ -11,38 +11,24 @@
 // Optimizado para no usar división (equivalencia de 1/10 con 0.1)
 #define FORMULA_TIEMPO (jugador->peso_total + 1) * 0.1
 
+unsigned short es_final = 0;
+
 //--- Funciones Principales del Jugador ---//
 
 void jugador_recoger(Player*);
 void jugador_descartar(Player*);
 void jugador_avanzar(Player*, Map*);
 void resetear_partida(Player*, Map*);
-int  salir_partida(void);
+void salir_partida(char*);
 
 //--- Funciones Varias del Jugador ---//
+
 Player* inicializar_jugador(void);
 void jugador_default(Player*);
 Item* item_buscar(List*, const char*);
 void jugador_actualizar_items(Player*, List*, Item*, const int);
 
 // --- //
-
-Item* item_buscar(List* items, const char* nombre) {
-    Item* item;
-    for (CADA_RECORRIDO(item, items)) {
-        if (!strcmp(item->nombre, nombre)) return item;
-    }
-    return NULL;
-}
-
-void jugador_actualizar_items(Player* jugador, List* lista, Item* item, const int signo) {
-    if (signo > 0) list_pushBack(jugador->items, item);
-    list_popCurrent(lista);
-
-    jugador->peso_total += signo * item->peso;
-    jugador->puntaje += signo * item->valor;
-    --(jugador->tiempo);
-}
 
 void jugador_recoger(Player* jugador) {
     State_Map* sala = jugador->sala_actual;
@@ -63,7 +49,7 @@ void jugador_descartar(Player* jugador) {
     if (list_first(jugador->items) == NULL) { puts("*Intentas abrir tu mochila para descargar objetos, pero no tienes ninguno*"); return; }
 
     mostrar_items(jugador->items);
-    char entrada[100];
+    char entrada[200];
     leer_entrada(entrada);
     
     Item* items_jugador = item_buscar(jugador->items, entrada);
@@ -77,13 +63,16 @@ void jugador_avanzar(Player* jugador, Map* mapa_juego) {
     limpiar_pantalla();
     imprimir_separador("Decides ver las habitaciones adjuntas a la actual", 50);
     mostrar_conexiones(jugador->sala_actual->adj_nodes);
+
+    // --- //
     Map* conexiones = map_create(is_equal_str);
-    State_Map* lista_conexiones = list_first(jugador->sala_actual->adj_nodes);
-    while (lista_conexiones != NULL) {
+    State_Map* lista_conexiones;
+    for (CADA_RECORRIDO(lista_conexiones, jugador->sala_actual->adj_nodes)) {
         map_insert(conexiones, lista_conexiones->nombre, lista_conexiones);
-        lista_conexiones = list_next(jugador->sala_actual->adj_nodes);
     }
-    char entrada[100];
+
+    // --- //
+    char entrada[200];
     puts("¿A qué habitación decides moverte? (Ninguna: \"0\")");
     MapPair* a;
     do {    
@@ -91,23 +80,35 @@ void jugador_avanzar(Player* jugador, Map* mapa_juego) {
         if (!strcmp(entrada, "0")) { puts("*Decides quedarte en la misma habitación*"); return; }
     } while((a = map_search(conexiones, entrada)) == NULL);
 
+    // --- //
     limpiar_pantalla();
     jugador->sala_actual = a->value;
-    printf("Decides moverte a esta habitación: \033[1;37m%s\033[0m\n", jugador->sala_actual->nombre);
-    jugador->tiempo -= FORMULA_TIEMPO;
+    if (jugador->sala_actual->final == 'S') { 
+        mostrar_mensaje_final(jugador);
+        es_final = 1;
+    } else {
+        printf("Decides moverte a esta habitación: \033[1;37m%s\033[0m\n", jugador->sala_actual->nombre);
+        jugador->tiempo -= FORMULA_TIEMPO;
+    }
     free(conexiones);
     return;
 }
 
-void jugador_default(Player* jugador){
-    jugador->tiempo = _TIEMPO;
-    jugador->sala_actual = NULL;
-    jugador->puntaje = 0;
-    jugador->peso_total = 0;
-    (jugador->items == NULL) ? jugador->items = list_create() : list_clean(jugador->items);
-}
-
 void resetear_partida(Player* jugador, Map* mapa_juego) {
+    limpiar_pantalla();
+    imprimir_separador("¿Quierés resetear la partida actual (PERDERÁS TODO EL PROGRESO)? [S/N]", 75);
+    char o;
+    leer_opcion(&o);
+    if (o == 'N') {
+        puts("Decidiste quedarte en la misma partida.");
+        return;
+    } else if (o != 'S') {
+        puts("*Te das cuenta que decidir fue muy abrumador*");
+        puts("*No haces nada*");
+        return;
+    }
+
+    // --- //
     map_clean(mapa_juego);
     mapa_cargado = 0;
     leer_mapa_completo(mapa_juego);
@@ -117,12 +118,17 @@ void resetear_partida(Player* jugador, Map* mapa_juego) {
     mostrar_reseteo();
 }
 
-int salir_partida(void) {
+void salir_partida(char* o) {
     limpiar_pantalla();
-    imprimir_separador("¿Estás seguro de querer salir de la partida?", 60);
-    char o;
-    leer_opcion(&o);
-    return (o == 'S') ? 1 : 0; 
+    imprimir_separador("¿Estás seguro de querer salir de la partida? [S/N]", 60);
+    leer_opcion(o);
+    if (*o == 'S') {
+        *o = '0';
+        imprimir_separador("FIN DE LA PARTIDA", 60);
+    } else if (*o != 'N') {
+        puts("*Intentaste pensar si salir valdría la pena o no*");
+        puts("*No haces nada*");
+    } else puts("No decidiste sucumbir a la tentación.");
 }
 
 void casos_opciones(char* o, Player* jugador, Map* mapa_juego) {
@@ -132,12 +138,13 @@ void casos_opciones(char* o, Player* jugador, Map* mapa_juego) {
         case '3': { jugador_avanzar(jugador, mapa_juego); *o = '\0'; break; }
         case '4': { VER_ESTADO(NULL); break; }
 
-        case '5': { resetear_partida(jugador, mapa_juego); break; }
-        case '*': { if (salir_partida()) { puts("FIN DE LA PARTIDA"); break; } }
+        case '-': { resetear_partida(jugador, mapa_juego); break; }
+        case '0': { salir_partida(o); break; }
         // Respuesta predeterminada
         default: { puts("*Intentaste usar una opción no disponible*\n*No surgió efecto*"); *o = 'd'; }
     }
-    if (*o != '*') esperar_enter();
+    if (*o != '0' && !es_final) esperar_enter();
+    if (*o == '\0' && !es_final) { VER_ESTADO(jugador); esperar_enter(); }
 }
 
 void pantalla_jugador(Player* jugador, Map* mapa_juego) {
@@ -146,12 +153,38 @@ void pantalla_jugador(Player* jugador, Map* mapa_juego) {
 
   char o = '\0';
   do {
-    if (o == '\0') { VER_ESTADO(jugador); esperar_enter(); }
     mostrar_menu_jugador();
     leer_opcion(&o);
     casos_opciones(&o, jugador, mapa_juego);
-  } while (o != '*' && jugador->tiempo > 0);
+  } while (o != '0' && jugador->tiempo > 0 && !es_final);
+
+  if (!(jugador->tiempo > 0)) mostrar_mensaje_derrota();
   return;
+}
+
+Item* item_buscar(List* items, const char* nombre) {
+    Item* item;
+    for (CADA_RECORRIDO(item, items)) {
+        if (!strcmp(item->nombre, nombre)) return item;
+    }
+    return NULL;
+}
+
+void jugador_actualizar_items(Player* jugador, List* lista, Item* item, const int signo) {
+    if (signo > 0) list_pushBack(jugador->items, item);
+    list_popCurrent(lista);
+
+    jugador->peso_total += signo * item->peso;
+    jugador->puntaje += signo * item->valor;
+    --(jugador->tiempo);
+}
+
+void jugador_default(Player* jugador){
+    jugador->tiempo = _TIEMPO;
+    jugador->sala_actual = NULL;
+    jugador->puntaje = 0;
+    jugador->peso_total = 0;
+    (jugador->items == NULL) ? jugador->items = list_create() : list_clean(jugador->items);
 }
 
 Player* inicializar_jugador(void) {
